9825f59dd8ccbd529cd290308999ab9f774bafad
이정환 <hwanlee2259@gmail.com>
2026-05-07 21:47:43 +0900

    1. seeing right only smile image

--- src/main.cpp
+++ src/main.cpp
@@ -94,6 +94,7 @@ int main() {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
+    stbi_image_free(data);
 
     unsigned int VAO1;
     unsigned int EBO;

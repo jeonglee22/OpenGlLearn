3f819ea9ec274550d5f0fb2022f76f29ef245bc8
이정환 <hwanlee2259@gmail.com>
2026-05-15 20:04:06 +0900

    gouraud shading - calculate in vs

--- Scripts/Shaders/object.fs
+++ Scripts/Shaders/object.fs
@@ -1,31 +1,34 @@
 #version 330 core
 out vec4 FragColor;
   
-uniform vec3 lightColor;
+// uniform vec3 lightColor;
 uniform vec3 objectColor;
 
-in vec3 Normal;
-in vec3 FragPos;
-in vec3 LightPos;
+//in vec3 Normal;
+//in vec3 FragPos;
+//in vec3 LightPos;
+
+in vec3 LightingColor;
 
 void main()
 {
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
+    // float ambientStrength = 0.1;
+    // vec3 ambient = ambientStrength * lightColor;
 
-    vec3 norm = normalize(Normal);
-    vec3 ligthDir = normalize(LightPos - FragPos);
+    // vec3 norm = normalize(Normal);
+    // vec3 ligthDir = normalize(LightPos - FragPos);
 
-    float diff = max(dot(ligthDir, norm), 0.0);
-    vec3 diffuse = diff * lightColor;
+    // float diff = max(dot(ligthDir, norm), 0.0);
+    // vec3 diffuse = diff * lightColor;
 
-    float specularStrength = 0.5;
-    vec3 viewDir = normalize(-FragPos);
-    vec3 reflectDir = reflect(-ligthDir, norm);
+    // float specularStrength = 0.5;
+    // vec3 viewDir = normalize(-FragPos);
+    // vec3 reflectDir = reflect(-ligthDir, norm);
 
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor; 
+    // float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    // vec3 specular = specularStrength * spec * lightColor; 
 
-    vec3 result = (ambient + diffuse + specular) * objectColor;
+    // vec3 result = (ambient + diffuse + specular) * objectColor;
+    vec3 result = LightingColor * objectColor;
     FragColor = vec4(result, 1.0);
 }
